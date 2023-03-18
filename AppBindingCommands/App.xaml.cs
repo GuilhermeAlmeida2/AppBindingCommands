f06c5bd4a21d5a896e8245893caa6f70ba0d3f6c
GuilhermeAlmeida <guilhermeamorimasilva@gmail.com>
2023-03-18 09:06:34 -0300

    Add project files.

---
+++ AppBindingCommands/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace AppBindingCommands;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		DateTime data = DateTime.Now;
+		Preferences.Set("dtAtual", data);
+		Preferences.Set("AcaoInicial", string.Format("* App execultado às {0}. \n", data));
+		MainPage = new AppShell();
+	}
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+		Preferences.Set("AcaoStart", string.Format("* App iniciada às {0}. \n", DateTime.Now));
+	}
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+        Preferences.Set("AcaoSleep",string.Format("* App em segundo pleno às {0}. \n",DateTime.Now));
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+        Preferences.Set("AcaoResume",string.Format("* App reativo às {0}. \n",DateTime.Now));
+    }
+}
